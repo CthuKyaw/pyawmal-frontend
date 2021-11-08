a6f401482c199f509e2b56f86e0af2b8bfdbec0c
Dolphin <cthukyaw13@gmail.com>
2021-11-08 18:17:45 +0630

    Update .env rebuild

--- src/components/video-chat/Socket.js
+++ src/components/video-chat/Socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
-const sockets = io('http://localhost:3002');
+const sockets = io(`${process.env.REACT_APP_SOCKET_IO_PATH}`);
 export default sockets;
